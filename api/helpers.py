e239387c9809bd0849a4026ecb08cf5705caa713
Fred Campos <mail2frede@gmail.com>
2020-08-24 06:54:07 -0300

    new helper function for calculate age

--- api/helpers.py
+++ api/helpers.py
@@ -14,3 +14,14 @@ def convert_str_date_to_object(date: str) -> datetime:
     :return: datetime object
     """
     return datetime.strptime(date, VALID_DATE_FORMAT)
+
+
+def calculate_age_by_date(birthdate: datetime) -> int:
+    """Calculate age by birthdate.
+
+    :param birthdate: A datetime object containing birth date.
+    :return: return calculated age
+    """
+    today = datetime.today()
+    diff = ((today.month, today.day) < (birthdate.month, birthdate.day))
+    return today.year - birthdate.year - diff
