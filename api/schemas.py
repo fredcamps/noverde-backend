a851f8881d738c22a3407828a6344be7c418c8a3
Fred Campos <mail2frede@gmail.com>
2020-08-24 06:59:59 -0300

    removed unused schemas

--- api/schemas.py
+++ api/schemas.py
@@ -3,15 +3,13 @@
 schemas.py
 """
 from decimal import Decimal
-from typing import List
 
-from schema import And, Or, Schema
+from schema import And, Schema, Use
 
 from api.validators import (
     validate_amount,
     validate_birthdate,
     validate_cpf,
-    validate_terms,
 )
 
 PostLoanRequest = Schema(
@@ -19,49 +17,8 @@ PostLoanRequest = Schema(
         'name': And(str, len),
         'cpf': And(str, validate_cpf),
         'birthdate': And(str, validate_birthdate),
-        'amount': And(Decimal, validate_amount),
-        'terms': And(int, validate_terms),
-        'income': And(Decimal, lambda inc: inc >= 1),
-    },
-)
-
-PostLoanSucessResponse = Schema(
-    {
-        'id': str,
-    },
-)
-
-PostLoanErrorResponse = Schema(
-    {
-        'errors': List,
-    },
-)
-
-GetLoanResponse = Schema(
-    {
-        'id': str,
-        'status': str,
-        'result': Or(str, None),
-        'refused_policy': Or(str, None),
-        'amount': Or(Decimal, None),
-        'terms': Or(int, None),
-    },
-)
-
-PostScoreRequest = Schema(
-    {
-        'cpf': And(str, validate_cpf),
-    },
-)
-
-PostCommitmentRequest = Schema(
-    {
-        'cpf': And(str, validate_cpf),
-    },
-)
-
-DefaultResponse = Schema(
-    {
-        'message': str,
+        'amount': And(Use(Decimal), validate_amount),
+        'terms': And(Use(int), lambda ter: ter >= 1),
+        'income': And(Use(Decimal), lambda inc: inc >= 1),
     },
 )
